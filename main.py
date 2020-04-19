d5971594b9b34f8e402cb9447ce32caf71569002
hoangcaominh <hoangcaominh1812@gmail.com>
2020-04-19 12:07:50 +0700

    Use threading

--- main.py
+++ main.py
@@ -1,3 +1,68 @@
+<<<<<<< HEAD
+import sys, os, threading
+import util
+
+if sys.platform != 'win32':
+	print('This command line tool cannot be used outside of Windows environment.')
+	exit()
+
+args = sys.argv[1:]
+
+if args:
+    util.load()
+    flag = args[0]
+    if flag == '-n' or flag == '--new':
+        if len(args) < 2:
+            util.error(1)
+        elif len(args) < 3:
+            util.error(2)
+        elif util.exist(args[1]):
+            util.error(8, args[1])
+        else:
+            util.opt_new(args[1], args[2])
+    elif flag == '-o' or flag == '--open':
+        if len(args) < 2:
+            util.error(1)
+        elif not util.exist(args[1]):
+            util.error(7, args[1])
+        else:
+            thread = threading.Thread(target=util.opt_open, args=(args[1],))
+            thread.start()
+    elif flag == '-i' or flag == '--info':
+        if len(args) < 2:
+            util.error(1)
+        elif not util.exist(args[1]):
+            util.error(7, args[1])
+        else:
+            util.opt_info(args[1])
+    elif flag == '-l' or flag == '--list':
+        util.opt_list()
+    elif flag == '-m' or flag == '--modify':
+        if len(args) < 2:
+            util.error(4)
+        elif len(args) < 3:
+            util.error(1)
+        elif len(args) < 4:
+            util.error(3)
+        elif not util.exist(args[2]):
+            util.error(7, args[2])
+        else:
+            util.opt_modify(args[1], args[2], args[3])
+    elif flag == '-d' or flag == '--delete':
+        if len(args) < 2:
+            util.error(1)
+        elif not util.exist(args[1]):
+            util.error(7, args[1])
+        else:
+            util.opt_delete(args[1])
+    elif flag == '-b' or flag == '--base':
+            print(os.path.dirname(__file__))
+    else:
+        util.error(6, args[0])
+    util.save(util.data)
+else:
+    print(util.usage)
+=======
 import sys, os
 import util
 
@@ -54,3 +119,4 @@ if args:
     util.save(util.data)
 else:
     print(util.usage)
+>>>>>>> b3354a5aa9411be755f76b4912f84165ce46b7b1
--- util.py
+++ util.py
@@ -1,3 +1,100 @@
+<<<<<<< HEAD
+import os, json
+
+usage = '''
+Usage:
+python "path_to_this_folder/main.py" [Option]
+
+Option:
+-n --new <name> <path>: Create a new shortcut
+-o --open <name>: Open a shortcut
+-i --info <name>: View the information of a shortcut
+-l --list: View the list of shortcuts
+-m --modify [-n --name][-p --path] <name> <change>: Modify the name and/or the path of a shortcut
+-d --delete <name>: Delete a shortcut
+-b --base: Show the directory in which this script is saved
+'''
+
+path = os.path.join(os.path.dirname(__file__), 'data.json')
+
+data = {}
+
+def exist(name):
+    return name in data
+
+def data_exist():
+    if not os.path.isfile(path):
+        with open(path, 'w') as file:
+            file.write('{}')
+            print('Initialized new data.')
+
+def load():
+    global data
+    data_exist()
+    with open(path, 'r') as file:
+        data = json.loads(file.readline())
+
+def save(data):
+    data_exist()
+    with open(path, 'w') as file:
+        file.write(json.dumps(data, sort_keys=True))
+
+def error(id, msg=''):
+    error_list = {
+        1: 'Argument <name> is not specified.',
+        2: 'Argument <path> is not specified.',
+        3: 'Argument <change> is not specified.',
+        4: 'Modification option is not specified.',
+        5: 'Unrecognized modification option: ' + msg + '.',
+        6: 'Unrecognized flag: ' + msg + '.',
+        7: '"' + msg + '" is not found in database.',
+        8: '"' + msg + '" already exists.',
+        9: 'Name cannot exceed 15 characters.'
+    }
+
+    print(error_list.get(id, 'Unknown error.'))
+    if id in [1, 2, 3, 4, 5]:
+        print(usage)
+
+def opt_new(name, path):
+    if len(name) < 16:
+        data[name] = path
+        # print('Added element ' + name + '.')
+    else:
+        error(9)
+
+def opt_open(name):
+    # print('Opening element ' + name + '...')
+    os.startfile(data[name])
+
+def opt_info(name):
+    print('Name: ' + name)
+    print('Path: ' + data[name])
+
+def opt_list():
+    print('Name\t\tPath')
+    print('-' * 80)
+    for key in data:
+        print(key + ('\t\t' if len(key) < 8 else '\t') + data[key])
+
+def opt_modify(option, name, change):
+    if option == '-n' or option == '--name':
+        if exist(change):
+            error(8, change)
+        elif len(change) > 15:
+            error(9)
+        else:
+            data[change] = data.pop(name)
+    elif option == '-p' or option == '--path':
+        data[name] = change
+    else:
+        error(5, option)
+
+def opt_delete(name):
+    del data[name]
+    # print('Deleted element ' + name + '.')
+
+=======
 import os, json
 
 usage = '''Usage:
@@ -82,3 +179,4 @@ def opt_modify(option, name, change):
 def opt_delete(name):
     del data[name]
     # print('Deleted element ' + name + '.')
+>>>>>>> b3354a5aa9411be755f76b4912f84165ce46b7b1
